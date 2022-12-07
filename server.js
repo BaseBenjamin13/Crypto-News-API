4286bdfe23fad6b2ba1ef1e4980da7b10dc0fd3d
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-12-07 16:41:48 -0500

    getting titles and urls using cheerio

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ app.get('/', (req, res) => {
     res.json({message: 'Hello there, welcome to Crypto News.'})
 })
 
-app.get('/news', async (req, res) => {
-    await axios.get("https://www.coindesk.com")
+app.get('/news', (req, res) => {
+    const news = [];
+
+    axios.get('https://www.coindesk.com/markets/')
         .then(data => {
-            res.json({news: data.data})
+            const html = data.data;
+            const $ = cheerio.load(html);
+            $('a:contains("crypto"), a:contains("coin")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                news.push({ title, url })
+            })
+            res.json(news)
         })
         .catch(err => console.log(err))
-    // const rese = await axios.get("https://www.coindesk.com")
-    // res.json({data: JSON.stringify(rese.data)})
 })
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
