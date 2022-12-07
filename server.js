ee1102a0c06ceaf9e9d26363c05ca5c79cafd00c
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-12-07 10:36:39 -0500

    setup news route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 
 const app = express(); 
 
@@ -10,7 +11,15 @@ app.get('/', (req, res) => {
     res.json({message: 'Hello there, welcome to Crypto News.'})
 })
 
-
+app.get('/news', async (req, res) => {
+    await axios.get("https://www.coindesk.com")
+        .then(data => {
+            res.json({news: data.data})
+        })
+        .catch(err => console.log(err))
+    // const rese = await axios.get("https://www.coindesk.com")
+    // res.json({data: JSON.stringify(rese.data)})
+})
 
 const PORT = 5050;
 app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
